b25c1fc1a8110089ae735da656f4182bc9bf5aef
inxane-rudrakxh <rudraksh7709@gmail.com>
2026-06-06 11:14:27 +0530

    seo

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -82,6 +82,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
       { property: "og:type", content: "website" },
       { name: "twitter:card", content: "summary_large_image" },
       { name: "theme-color", content: "#0f1721" },
+      { name: "google-site-verification", content: "NQfkUyHbhxWiwnKXHWtN2ts-XxjmMCQFkRtc_I-9sPE" },
     ],
     links: [
       { rel: "stylesheet", href: appCss },
